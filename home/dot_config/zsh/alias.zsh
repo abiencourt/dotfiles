587f29dea17c159d9e5676bf917d131a4806bd88
Adrien Biencourt <adrien@bncrt.com>
2025-07-09 21:40:54 +0100

    fix(kitty): alias `ssh` command to avoid kitty issues

--- home/dot_config/zsh/alias.zsh
+++ home/dot_config/zsh/alias.zsh
@@ -1,5 +1,25 @@
 # global
 alias grep="grep --color"
 
+# https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+# Define a function that prompts for input
+ssh_prompt() {
+    if [ "$TERM" = "xterm-kitty" ]; then
+        echo -n "Do you want to use kitten ssh (y/n)? "
+        read choice
+        case "$choice" in
+        y | Y) kitty +kitten ssh "$@" ;;
+        *) command ssh "$@" ;;
+        esac
+    else
+        command ssh "$@"
+    fi
+}
+
+# Alias the ssh command to use the prompt function
+alias ssh="ssh_prompt"
+
 # Disable zsh autocorrect for aider
 alias aider='nocorrect aider'
